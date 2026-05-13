4b42f627fd9c7d75ab9c71e6a754de057d41b591
TayoVan <ksiunya.s@gmail.com>
2026-05-13 17:22:49 +0300

    Add files via upload

--- UserIdentifier.java
+++ UserIdentifier.java
@@ -2,31 +2,33 @@ import java.util.Objects;
 
 public class UserIdentifier {
     private int id;
-    public String name;
+    private String name;
 
     public UserIdentifier(int id, String name) {
         this.id = id;
         this.name = name;
     }
+
     public int getId() {
         return id;
     }
-    public void setId(int id) {
-        this.id = id;
+
+    public String getName() {
+        return name;
     }
+
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-
-        if (!(o instanceof UserIdentifier that)) return false;
-
-        return id == that.id &&
-                Objects.equals(name, that.name);
+        if (!(o instanceof UserIdentifier)) return false;
+        UserIdentifier userIdentifier = (UserIdentifier) o;
+        return id == userIdentifier.id && Objects.equals(name, userIdentifier.name);
     }
+
     @Override
     public int hashCode() {
         return Objects.hash(id, name);
     }
+
     @Override
     public String toString() {
         return "ID = " + id +
